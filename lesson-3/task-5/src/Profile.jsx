b5264e7dc08877810e7c73f9c1d9ea236bf451dc
FastikDev <sir.fastik18@gmail.com>
2024-07-06 16:36:02 +0300

    format date

--- lesson-3/task-5/src/Profile.jsx
+++ lesson-3/task-5/src/Profile.jsx
@@ -4,18 +4,19 @@ import moment from 'moment';
 const formatDate = date => moment(date).format('DD MMM YY');
 
 const Profile = props => {
-  const { userData } = props; 
-  
+  const { userData } = props;
+
   return (
     <div className="profile">
       <div className="profile__name">
-        {userData.firstName} {userData.lastName}
+        {`firstName=${userData.firstName} lastName=${userData.lastName}`}
       </div>
       <div className="profile__birth">
-        Was born {formatDate(userData.birthDate)} {userData.birthPlace}
+        {`Was born birthDate=${formatDate(userData.birthDate)} in birthPlace=${userData.birthPlace}`}
       </div>
     </div>
   );
 };
 
 export default Profile;
+
