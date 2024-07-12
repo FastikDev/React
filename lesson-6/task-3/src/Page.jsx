3e68d81e242a0f3c55b7c6feb35e1aa54f174d7a
FastikDev <sir.fastik18@gmail.com>
2024-07-12 12:27:31 +0300

    Info about products

---
+++ lesson-6/task-3/src/Page.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import Info from './Info';
+
+const text1 = 'Iphone 13 - Price is 500$. Available in 2 colors';
+const text2 = 'Iphone 13 Pro - Price is 650$. Not Available';
+
+class Page extends Component {
+  state = {
+    text: null
+  }
+
+  setText = text => () => {
+    this.setState({
+      text 
+    });
+  }
+
+  render () {
+    return (
+      <div className="page">
+        <Info text={this.state.text}/>
+        <div className="actions">
+          <button className="btn" onClick={this.setText(text1)}>IPhone 13</button>
+          <button className="btn" onClick={this.setText(text2)}>IPhone 13 Pro</button>
+          <button className="btn" onClick={this.setText('')}>Clear</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Page;
