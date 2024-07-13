cdd1b89a2886be6cd01681f6052ec96324c00d74
Viktor Troyan <149237046+FastikDev@users.noreply.github.com>
2024-07-13 18:29:58 +0300

    Update index.jsx

--- lesson-7/task-3/src/index.jsx
+++ lesson-7/task-3/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TransactionsList from './TransactionsList';
-import transactions from './transactions.js';
+import transactions from './transactions';
 import './index.scss';
 
 const rootElement = document.querySelector('#root');
