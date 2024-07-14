f8f143a6ab309657240b2346cb721d1ea852092e
Viktor Troyan <149237046+FastikDev@users.noreply.github.com>
2024-07-14 07:34:41 +0300

    Update Transaction.jsx

--- lesson-7/task-3/src/Transaction.jsx
+++ lesson-7/task-3/src/Transaction.jsx
@@ -10,7 +10,7 @@ const Transaction = ({ from, to, amount, rate, time }) => {
     <li className="transaction">
       <span className="transaction__date">{formattedDate}</span>
       <span className="transaction__time">{formattedTime}</span>
-       <span className="transaction__assets">{'${from} → ${to}'}</span>
+      <span className="transaction__assets">{`${from} → ${to}`}</span>
       <span className="transaction__rate">{rate}</span>
       <span className="transaction__amount">{formattedAmount}</span>
     </li>
