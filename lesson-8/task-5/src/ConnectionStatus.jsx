0204e925bbeab2632d69b3f846a7f0185866307e
AlinkaSuper <sir.fastik18@gmail.com>
2024-07-17 13:22:05 +0300

    online/offline

---
+++ lesson-8/task-5/src/ConnectionStatus.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+class ConnectionStatus extends React.Component {
+    state = {
+        status: 'Online',
+    };
+
+    componentDidMount() {
+        window.addEventListener('online', this.updateNetworkStatus);
+        window.addEventListener('offline', this.updateNetworkStatus);
+        this.updateNetworkStatus();
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('online', this.updateNetworkStatus);
+        window.removeEventListener('offline', this.updateNetworkStatus);
+    }
+
+    updateNetworkStatus = () => {
+        this.setState({
+            status: navigator.onLine ? 'Online' : 'Offline'
+        });
+    }
+
+    render() {
+        const { status } = this.state;
+        const statusClass = status === 'Offline' ? 'status_offline' : '';
+
+        return (
+            <div className={`status ${statusClass}`}>{status}</div>
+        );
+    }
+}
+
+export default ConnectionStatus;
