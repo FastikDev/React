cbf33b3c12d54ae23d20e989ef9580397a470e89
FastikDev <sir.fastik18@gmail.com>
2024-07-27 02:14:56 +0300

    filter list

---
+++ lesson-9/task-4/src/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import UsersList from "./UsersList";
+import './index.scss';
+
+const users = [
+    {name: 'Tad', age: 18},
+    {name: 'Anna', age: 45},
+    {name: 'Jhon', age: 30},
+    {name: 'Alice', age: 25},
+    {name: 'Bob', age: 35}
+];
+
+const root = document.querySelector('#root');
+
+ReactDOM.render(
+    <React.StrictMode>
+        <UsersList users={users} />
+    </React.StrictMode>, 
+    root
+);
