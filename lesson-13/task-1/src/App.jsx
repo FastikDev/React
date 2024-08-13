ffbfae95fd7054720fa7a9c33654e4797839ee3f
FastikDev <sir.fastik18@gmail.com>
2024-08-13 21:29:55 +0300

    fix page

--- lesson-13/task-1/src/App.jsx
+++ lesson-13/task-1/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Contacts from './Contacts';
 import Products from './Products';
@@ -6,26 +6,26 @@ import Home from './Home';
 import PageNotFound from './PageNotFound';
 
 const App = () => {
-    return (
-        <div className="page">
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                    <Route path="/products">
-                        <Products />
-                    </Route>
-                    <Route path="/contacts">
-                        <Contacts />
-                    </Route>
-                    <Route path="*">
-                        <PageNotFound />
-                    </Route>
-                </Switch>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div className="page">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/products">
+            <Products />
+          </Route>
+          <Route path="/contacts">
+            <Contacts />
+          </Route>
+          <Route path="*">
+            <PageNotFound />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
