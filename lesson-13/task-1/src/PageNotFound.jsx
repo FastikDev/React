ca27f329c97baab560390e9e5033facc2ee5590b
AlinkaSuper <sir.fastik18@gmail.com>
2024-08-13 21:14:22 +0300

    Router intro

---
+++ lesson-13/task-1/src/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+const PageNotFound = () => {
+    return (
+        <div className="page__content">
+            <h1>😢</h1>
+            <Link to="/">Go home</Link>
+        </div>
+    );
+}
